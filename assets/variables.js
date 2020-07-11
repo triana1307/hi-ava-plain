55de7f705c8b912042e8015687efe1d6a205d957
triana1307 <51686163+triana1307@users.noreply.github.com>
2020-07-11 18:01:54 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -7,7 +7,7 @@ const variables = {
     night: "Good Night",
   },
   quotes: [
-    { quote: "I'm sorry for loving you.", author: "standinhsd" },
+    { quote: "I'm sorry for loving you.", author: "trianajuliansah" },
     { quote: "I love you.", author: "Trian" },
     { quote: "Aku sayang kamu!", author: "tria" },
     { quote: "Aku sayang kamu ♥", author: "triana-Nya!" },
